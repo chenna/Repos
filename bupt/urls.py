b307230ee256a6f13cffc34d3015022dc140825f
chenna <earth_china007@163.com>
2014-11-22 19:44:45 +0800

    About Template
    Template created or updated.

--- bupt/urls.py
+++ bupt/urls.py
@@ -7,12 +7,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bupt.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tools/$', 'tools.views.index', name='home'),
+    url(r'^tools/$', 'tools.views.index'),
                        
-    url(r'^$', home),
+    url(r'^$', home, name='home'),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
